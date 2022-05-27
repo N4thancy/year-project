8920bf926b11b163083a6042709d309ef9f0917c
N4thancy <93983753+N4thancy@users.noreply.github.com>
2022-05-27 16:25:48 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,42 +1,43 @@
 import socket
 import threading
-
-# Standard size used for the header message
+import dbConverter
 header = 64
-# Local port and IP Socket settings
-port = 5050 # Pick a port
+port = 5050
 ip = socket.gethostbyname(socket.gethostname())
-print(ip) #Printing the host IP for use on client
+print(ip)
 addr = (ip, port)
 
-# Format for encoding and decoding messages
+class Data:
+	new_list = []
+
 format = "utf-8"
 
-# Server socket object defined
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(addr) # Socket bound to local address
+server.bind(addr)
 
-# Server handling new connections and messages
 def handle_client(conn, addr):
-    print("New Connection") # Debug
-    connection = True
-    while connection is True: # To add connection closed later. 
-        # First message is recived and decoded 
-        msg_one =conn.recv(header).decode.format()
-        if msg_one: # if messages isn't null run the following
-            msg_length = int(msg_one) # The main message lenght is defined as int from the decoded message
-            msg = conn.recv(msg_length).decode(format) # Main message is recieved with the defined length 
-            print(msg) # Debug
-            print(msg_length) # Debug
+	print("NEW CONNECTION")
+	connection = True
+	while connection is True:
+		msg_one = conn.recv(header).decode(format)
+		if msg_one:
+			msg_length = int(msg_one)
+			msg = conn.recv(msg_length).decode(format)
+			print(msg)
+			print(msg_length)
+			Data.new_list = msg.split("/")
+			for x in Data.new_list:
+				dbConverter.barcode_Split(x)
 
-# Main function starts and listens for new devices 
+			
+
 def start():
-    server.listen() # Server object listens for new connections
-    while True:
-        conn, addr = server.accept() # New connection accepted and variables defined
-        thread = threading.Thread(target=handle_client, args=(conn, addr)) # New connections varriables, used as arguments for handle function as a thread
-        thread.start() # The handle client thread is started so start() can keep listening for new connections
-        
+	server.listen()
+	while True:
+		conn, addr = server.accept()
+		thread = threading.Thread(target=handle_client, args=(conn, addr))
+		thread.start()
+		
 
-print("Server is starting") # debug
-start() # Runs the main function
+print("Server starting")
+start()
