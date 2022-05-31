ecba90aa5c87915c9feef397807d2357dca96a95
N4thancy <93983753+N4thancy@users.noreply.github.com>
2022-05-31 16:12:34 +0200

    Update local_server.py

--- local_server.py
+++ local_server.py
@@ -8,7 +8,7 @@ header = 64
 # Local port and IP Socket settings
 port = 5050
 ip = "192.168.137.247"#socket.gethostbyname(socket.gethostname())
-print(ip) 
+print("LOCAL SERVER HOSTED ON: ", ip) 
 addr = (ip, port)
 
 format = "utf-8"
@@ -16,8 +16,9 @@ format = "utf-8"
 # Port and IP Socket settings defined for server
 Server_port = 5050 # Server's definer port
 server_ip = "20.91.176.137"
-print(server_ip) # Debug
+print("CONNECTING TO: ", server_ip) # Debug
 server_addr = (server_ip, Server_port)
+
 # Format for encoding and decoding messages
 format = "utf-8"
 
@@ -31,7 +32,7 @@ client.connect(server_addr)
 
 # Server handling new connections and messages
 def handle_client(conn, addr):
-    print("New Connection") # Debug
+    print("-" * 5, "New Connection", "-" * 5) # Debug
     connection = True
     while connection is True: # To add connection closed later. 
         # First message is recived and decoded 
@@ -39,10 +40,9 @@ def handle_client(conn, addr):
         if msg_one: # if messages isn't null run the following
             msg_length = int(msg_one) # The main message lenght is defined as int from the decoded message
             msg = conn.recv(msg_length).decode(format) # Main message is recieved with the defined length 
-            print(msg) # Debug
+            print("MESSAGE RECIEVED: ", msg) # Debug
             send(msg)
-            print(msg_length) # Debug
-
+            print("-" * 5, "MESSAGE SENT", "-" * 5)
 # Main function starts and listens for new devices 
 def start():
     server.listen() # Server object listens for new connections
@@ -51,7 +51,6 @@ def start():
         thread = threading.Thread(target=handle_client, args=(conn, addr)) # New connections varriables, used as arguments for handle function as a thread
         thread.start() # The handle client thread is started so start() can keep listening for new connections
 
-# Define function for sending message to created server instance
 def send(msg):
     # Message encoded 
     message = msg.encode(format)
@@ -66,7 +65,8 @@ def send(msg):
     time.sleep(0.05) # Time.sleep to create space between messages 
     # Send the main messages
     client.send(message)
-    print(msg) # Debug
-    print(msg_length) # Debug
+    print("-" * 5, "SENDING MSG TO: ", server_ip, "-" * 5)
+    print("SENT MESSAGE: ", msg) # Debug
+    print("THE MESSAGE LENGTH IS: ", msg_length) # Debug
 
 start() # Runs the main function
